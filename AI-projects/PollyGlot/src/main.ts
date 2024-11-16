ca25036327a1c490adc7b39e5a8c4f67a21bfcaa
Eyub197 <eyubmehmed122@gmail.com>
2024-11-16 06:36:51 +0200

    Found wrong value in html for Japanese option the value used to be France

--- AI-projects/PollyGlot/src/main.ts
+++ AI-projects/PollyGlot/src/main.ts
@@ -3,3 +3,12 @@ import './style.css'
 
 const genAi = new GoogleGenerativeAI(import.meta.env.VITE_GEMENI_AI_API_KEY)
 const model = genAi.getGenerativeModel({model: "gemini-1.5-pro"})
+const form = document.querySelector("form") 
+const userInput = document.querySelector(".user-input")
+
+
+form?.addEventListener("submit", event => {
+    event.preventDefault()
+    const formData = new FormData(form)
+    console.log(formData.get("language"))
+})
