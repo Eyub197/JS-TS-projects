6dd1b883a882d273ebcbabe9901d0299ef9b8851
Eyub197 <eyubmehmed122@gmail.com>
2024-11-16 06:46:56 +0200

    console translating ready. Next step rendering it

--- AI-projects/PollyGlot/src/main.ts
+++ AI-projects/PollyGlot/src/main.ts
@@ -5,10 +5,22 @@ const genAi = new GoogleGenerativeAI(import.meta.env.VITE_GEMENI_AI_API_KEY)
 const model = genAi.getGenerativeModel({model: "gemini-1.5-pro"})
 const form = document.querySelector("form") 
 const userInput = document.querySelector(".user-input")
+const translateButton = document.querySelector(".cta")
 
+
+const createPrompt = async () => {
+    const formData = new FormData(form!)
+    const prompt = (`Hello, can you translate this text ${formData.get("text")} into ${formData.get("language")} language? `)
+    const result = await model.generateContent(prompt)
+    console.log(result.response.text())
+    return result.response.text()
+}
 
 form?.addEventListener("submit", event => {
     event.preventDefault()
-    const formData = new FormData(form)
-    console.log(formData.get("language"))
+})
+
+translateButton?.addEventListener("click", event => {
+    event.preventDefault()
+    createPrompt()
 })
