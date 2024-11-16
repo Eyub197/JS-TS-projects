298f6bd7abb57092832e739b0766e97ff765127c
Eyub197 <eyubmehmed122@gmail.com>
2024-11-16 06:07:00 +0200

    Made connection with the ai model

--- AI-projects/PollyGlot/src/main.ts
+++ AI-projects/PollyGlot/src/main.ts
@@ -1,2 +1,5 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
 import './style.css'
 
+const genAi = new GoogleGenerativeAI(import.meta.env.VITE_GEMENI_AI_API_KEY)
+const model = genAi.getGenerativeModel({model: "gemini-1.5-pro"})
