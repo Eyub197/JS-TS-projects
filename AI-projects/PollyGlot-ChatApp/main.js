3857120a1382852c09a73aacf3af6a8d4cbf70bf
Eyub197 <eyubmehmed122@gmail.com>
2024-11-21 10:09:16 +0200

    Styled the ai message

--- AI-projects/PollyGlot-ChatApp/main.js
+++ AI-projects/PollyGlot-ChatApp/main.js
@@ -1,10 +1,24 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import './style.css'
 
+const form = document.querySelector("form") 
+
 const genAi = new GoogleGenerativeAI(import.meta.env.VITE_GEMENI_AI_API_KEY)
 const model = genAi.getGenerativeModel({model: "gemini-1.5-pro"})
 
-const form = document.querySelector("form") 
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts:[{text: "Can you help me with translating text" }]
+    },
+    {
+      role: "model",
+      parts:[{text: "Select the language you me to translate into, type your text and hit send!"}]
+    }
+  ]
+})
+
 
 
 // You need a method to create a  prompt  and add it to the dom and then to create a user element and add to the dom to 
@@ -17,7 +31,8 @@ const manageLoading = (isLoading) => {
 const createPrompt = async () => {
   //we need to add
   const formData = new FormData(form)
-  const prompt = (`Hello, can you translate this text ${formData.get("text")} into ${formData.get("language")} language? I want only the translated text as a response.`)
+  const userPrompt = await chat.sendMessage(`Hello, can you translate this text ${formData.get("text")} into ${formData.get("language")} language? `)
+  console.log(userPrompt.response.text())
   try {
       const result = await model.generateContent(prompt)
       return result.response.text()
@@ -35,5 +50,8 @@ const renderTranslation = () => {
 
 form?.addEventListener("submit", event => { event.preventDefault() })
 
+document.querySelector("button"),addEventListener("click", async () => {
+  await createPrompt()
+})
 
 
