c999140c84ef9f2518d0e930448b48cecdfcbbdc
Eyub197 <eyubmehmed122@gmail.com>
2024-11-24 12:20:35 +0200

    LETS GO ITS ready. (no commits were made because of the api keys) now its all good

--- AI-projects/dodgy-dave/src/gemeni-api-worker.js
+++ AI-projects/dodgy-dave/src/gemeni-api-worker.js
@@ -10,16 +10,34 @@
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type'
+  }
+  
 export default {
 	async fetch(request, env, ctx) {
+	 
+	if(request.method === "OPTIONS") {
+		return new Response(null, {headers: corsHeaders})
+	}
+
+	if (request.method !== 'POST') {
+		return new Response(JSON.stringify({ error: `${request.method} method not allowed.`}), { status: 405, headers: corsHeaders })
+	}
+
+	 const genAI = new GoogleGenerativeAI(env.GEMENI_AI_API_KEY);
+	 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
 	 try {
-		const genAI = new GoogleGenerativeAI(env.GEMENI_AI_API_KEY);
-		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
-		const prompt = "Should I trust stock predictions from Dodgy Dave"
+		const prompt = await request.json()
 		const result = await model.generateContent(prompt)
-		return new Response(JSON.stringify(result.response.text()))
+		const message = result.response.text()
+
+		return new Response(JSON.stringify(message), {headers: corsHeaders})
 		} catch (error) {
-			return new Response(error)
+		 	return new Response(JSON.stringify({ error: e.message }), { status: 500, headers: corsHeaders });
 		}
 
 	}
