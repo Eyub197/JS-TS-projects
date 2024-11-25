85e0c597650e2ace7b80c125cd29c04e45057936
Eyub197 <eyubmehmed122@gmail.com>
2024-11-25 13:53:24 +0200

    imported supabase and open ai

--- AI-projects/PopChoice/main.js
+++ AI-projects/PopChoice/main.js
@@ -1,1 +1,2 @@
 import './style.css'
+import { openai, supabase } from "./utils.js"
---
+++ AI-projects/PopChoice/utils.js
@@ -0,0 +1,19 @@
+import OpenAI from "openai"
+import { createClient } from "@supabase/supabase-js"
+
+if(!import.meta.env.VITE_OPENAI_API_KEY) { throw new Error("Missing OPEN_AI_API_KEY") }
+
+export const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+})
+
+const privateKey = import.meta.env.VITE_SUPABASE_API_KEY
+
+if(!privateKey) { throw new Error("Missing SUPABASE_API_KEY") }
+
+const url = import.meta.env.VITE_SUPABASE_URL
+
+if(!url) { throw new Error("Missing SUPABASE_URL") }
+
+export const supabase = createClient(url, privateKey)
