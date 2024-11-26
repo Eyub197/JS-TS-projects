01277d8928bd5900b50ba626b7af2e85496b349c
Eyub197 <eyubmehmed122@gmail.com>
2024-11-26 16:20:34 +0200

    This will run only ones it doesn't need to run every time lol

--- AI-projects/PopChoice/utils.js
+++ AI-projects/PopChoice/utils.js
@@ -20,33 +20,33 @@ if(!url) { throw new Error("Missing SUPABASE_URL") }
 
 export const supabase = createClient(url, privateKey)
 
-const splitDocument = async document => {
-    const response = await fetch(document)
-    const text = await response.text()
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 250,
-        chunkOverlap: 35,
-    })
-    const output = await splitter.createDocuments([text])
-    return output
-}
+// const splitDocument = async document => {
+//     const response = await fetch(document)
+//     const text = await response.text()
+//     const splitter = new RecursiveCharacterTextSplitter({
+//         chunkSize: 250,
+//         chunkOverlap: 35,
+//     })
+//     const output = await splitter.createDocuments([text])
+//     return output
+// }
 
-const createAndStoreEmbeddings = async () => {
-    const chunkData = await splitDocument('movies.txt')
-    const data = await Promise.all(
-        chunkData.map(async (chunk) => {
-          const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-002",
-            input: chunk.pageContent
-          })
+// const createAndStoreEmbeddings = async () => {
+//     const chunkData = await splitDocument('movies.txt')
+//     const data = await Promise.all(
+//         chunkData.map(async (chunk) => {
+//           const embeddingResponse = await openai.embeddings.create({
+//             model: "text-embedding-ada-002",
+//             input: chunk.pageContent
+//           })
 
-          return { 
-            content: chunk.pageContent, 
-            embedding: embeddingResponse.data[0].embedding 
-          }
-        })
-    )
-    await supabase.from('movies').insert(data)
-}
+//           return { 
+//             content: chunk.pageContent, 
+//             embedding: embeddingResponse.data[0].embedding 
+//           }
+//         })
+//     )
+//     await supabase.from('movies').insert(data)
+// }
 
-createAndStoreEmbeddings()
+// createAndStoreEmbeddings()
